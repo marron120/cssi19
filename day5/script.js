5ff27efe19ff7ca4a1a4ed7baa6898344b9bd5ad
marron120 <marronstmarthe@mec.science>
2019-07-29 12:01:06 -0400

    java

---
+++ day5/script.js
@@ -0,0 +1,24 @@
+console.log ("1");
+console.log ("2");
+console.log ("3");
+console.log ("4");
+console.log ("5");
+console.log ("6");
+console.log ("7");
+console.log ("8");
+console.log ("9");
+console.log ("10");
+
+
+
+
+
+let result = prompt("Enter your name");
+console.log("Hello ,",result); 
+let num= prompt ("Enter your grade");
+if (num>=70) {
+ console.log("You passed");
+} 
+else {
+    console.log("You failed");
+}
---
+++ day5/script2.js
@@ -0,0 +1,37 @@
+// 
+function f(x)
+{
+     return 3 * x + 7;
+     console.log ("done");
+}
+// Display the result of
+// calling the
+console.log (f(2));
+console.log (f(12));
+console.log (f(7));
+
+
+function g(x)
+{
+    return 5 * x + 5;
+}
+console.log (g(5));
+console.log (g(13));
+console.log (g(9));
+
+function i(x,y)
+
+{
+    return x*x - y*y;
+}
+console.log(i(3,4));
+console.log(i(4,3));
+console.log(i(5,2));
+
+function j(x,y,z)
+{
+    return x + y + z;
+}
+console.log (j(3,4,5))
+console.log (j(6,8,1))
+console.log (j(9,4,5))
